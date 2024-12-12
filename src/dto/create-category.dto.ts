43d1cc788ca5fcc2410ecf29e749dda6cbc6a937
tructt <truc.tran@yoong.vn>
2024-12-12 15:25:06 +0700

    update category

--- src/dto/create-category.dto.ts
+++ src/dto/create-category.dto.ts
@@ -4,13 +4,22 @@ export class CreateCategoryDto {
   @IsString()
   name: string;
 
-  // TODO: what is readonly?
   @IsString()
   @IsOptional()
   readonly description?: string;
 
-  // TODO: what is readonly?
   @IsUUID()
   @IsOptional()
   readonly parentId?: string;
 }
+
+/*
+
+// data payload
+{
+  "name": "category 1",
+  "description": "description 1",
+  "parentId": "d0b3b3b3-0b3b-4b3b-8b3b-0b3b3b3b3b3b"
+}
+
+*/
--- src/module/categories/categories.service.ts
+++ src/module/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoriesEntity } from './categories.entity';
 import { Repository } from 'typeorm';
@@ -11,13 +11,11 @@ export class CategoriesService {
   ) {}
 
   // create a new category
-  // TODO: should use CreateCategoryDto instead of Partial<CategoriesEntity>?
   async create(data: Partial<CategoriesEntity>): Promise<CategoriesEntity> {
     const category = this.categoriesRepository.create(data);
     return this.categoriesRepository.save(category);
   }
 
-  // TODO: what is "Load parent category?"
   // get all categories
   async findAll(): Promise<CategoriesEntity[]> {
     return this.categoriesRepository.find({
@@ -27,10 +25,16 @@ export class CategoriesService {
 
   // get category by id
   async findOne(id: string): Promise<CategoriesEntity> {
-    return this.categoriesRepository.findOne({
+    const result = await this.categoriesRepository.findOne({
       where: { id },
       relations: ['parent'], // Load parent category
     });
+    console.log('result', result);
+
+    if (!result) {
+      throw new NotFoundException(`result with ID ${id} not found`);
+    }
+    return result;
   }
 
   async update(
